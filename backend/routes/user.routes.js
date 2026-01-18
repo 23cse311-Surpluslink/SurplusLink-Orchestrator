802332bf01eff3cf00bd4401110e7cbdbc226a8c
Priyansh Narang <145008344+priyansh-narang2308@users.noreply.github.com>
2026-01-18 12:23:57 +0530

    feat: Added the profile route to update the profile

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,12 +2,14 @@ import express from 'express';
 import {
     getUserProfile,
     verifyUser,
+    updateUserProfile,
 } from '../controllers/user.controller.js';
 import { protect, roleBasedAccess } from '../middleware/auth.middleware.js';
 
 const userRouter = express.Router();
 
 userRouter.get('/profile', protect, getUserProfile);
+userRouter.put('/profile', protect, updateUserProfile);
 userRouter.patch('/verify', protect, roleBasedAccess(['admin']), verifyUser);
 
 export default userRouter;
