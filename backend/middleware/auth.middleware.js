3f38771abd0a8f05511afc5fed44d27ac9f3a394
Priyansh Narang <145008344+priyansh-narang2308@users.noreply.github.com>
2026-01-18 12:03:07 +0530

    feat: Completed user auth and signup

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -8,24 +8,24 @@ const protect = async (req, res, next) => {
         req.headers.authorization &&
         req.headers.authorization.startsWith('Bearer')
     ) {
+        token = req.headers.authorization.split(' ')[1];
+    } else if (req.cookies.token) {
+        token = req.cookies.token;
+    }
+
+    if (token) {
         try {
-            token = req.headers.authorization.split(' ')[1];
-
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
             req.user = await User.findById(decoded.id).select('-password');
-
             next();
         } catch (error) {
             console.error(error);
             res.status(401);
-            throw new Error('Not authorized, token failed');
+            next(new Error('Not authorized, token failed'));
         }
-    }
-
-    if (!token) {
+    } else {
         res.status(401);
-        throw new Error('Not authorized, no token');
+        next(new Error('Not authorized, no token'));
     }
 };
 
