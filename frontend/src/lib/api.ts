d86dd06b8a71def56f9a31fa01f827e87d75d004
Priyansh Narang <145008344+priyansh-narang2308@users.noreply.github.com>
2026-01-27 12:18:44 +0530

    Fixed

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/v1',
+  baseURL: import.meta.env.VITE_API_BASE_URL||"https://surpluslink-9fq6.onrender.com/api/v1 ",
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
