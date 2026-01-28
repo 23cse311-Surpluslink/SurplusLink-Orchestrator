66d41a1bf5b863d1a522b0ec3c9bf6deefa72c65
Sanjay <sanjaysj2005@gmail.com>
2026-01-28 22:09:01 +0530

    test: Donar testing completed

---
+++ backend/jest.config.js
@@ -0,0 +1,8 @@
+export default {
+    testEnvironment: 'node',
+    transform: {},
+    verbose: true,
+    testTimeout: 60000,
+    moduleFileExtensions: ['js', 'mjs', 'json', 'node'],
+    testMatch: ['**/tests/**/*.test.js'],
+};
