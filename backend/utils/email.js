4ec812149f9f6134682813502549e5a775eafbdc
Priyansh Narang <145008344+priyansh-narang2308@users.noreply.github.com>
2026-02-05 23:39:56 +0530

    Resolved in OTP

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -10,12 +10,14 @@ const sendEmail = async (options) => {
   }
 
   const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
+    service: 'gmail',
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
     },
+    tls: {
+      rejectUnauthorized: false
+    }
   });
 
   const mailOptions = {
