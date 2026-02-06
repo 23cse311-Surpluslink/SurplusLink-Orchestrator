b25bc76871449946ff7958b011ae2541ed05d12a
ArpithaShri <arpithashri.sudhakar@gmail.com>
2026-02-06 14:50:13 +0530

    NGO Acceptance & Utilization Report Implemented

--- backend/routes/report.routes.js
+++ backend/routes/report.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getDonationReport } from '../controllers/report.controller.js';
+import { getDonationReport, getNgoUtilizationReport } from '../controllers/report.controller.js';
 import { protect, roleBasedAccess } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
 router.get('/donations', protect, roleBasedAccess(['admin', 'donor']), getDonationReport);
+router.get('/ngo-utilization', protect, roleBasedAccess(['admin', 'ngo']), getNgoUtilizationReport);
 
 export default router;
