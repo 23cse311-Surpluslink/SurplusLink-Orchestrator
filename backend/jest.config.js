5c3576d66a7291bf0c02a64cb464c893885c8fc3
Priyansh Narang <145008344+priyansh-narang2308@users.noreply.github.com>
2026-02-10 21:48:32 +0530

    chore: sync final readme to orchestrator

--- backend/jest.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    testEnvironment: 'node',
-    transform: {},
-    verbose: true,
-    testTimeout: 60000,
-    moduleFileExtensions: ['js', 'mjs', 'json', 'node'],
-    testMatch: ['**/tests/**/*.test.js'],
-};
